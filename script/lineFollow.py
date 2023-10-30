498298b734737632376d5003ea9640bb6df07c0b
Ban-Ironic-Ohms <micahrobinson93@gmail.com>
2023-10-29 21:43:08 -0400

    documentaion :(

--- script/lineFollow.py
+++ script/lineFollow.py
@@ -1,23 +1,35 @@
+# HEADER
+# This allows us to follow a line. Currently, it follows a straight line only, 
+# but will include other line types in the future. 
+
+# ---------- IMPORTS ---------- #
 import time
 import grovepi
 import script.movement as mv
 
+# ---------- INITIALIZATION ---------- #
 # Connect the Grove Line Finder to digital port D7
 # SIG,NC,VCC,GND
 L_LF_PORT = 3
 R_LF_PORT = 2
 
+# idk why we need this
 grovepi.pinMode(L_LF_PORT,"INPUT")
 grovepi.pinMode(R_LF_PORT,"INPUT")
 
+# ---------- FUNCTIONS ---------- #
+# this function will follow the solid line using two line finders
 def followSolid(speed=10):
     while True:
+        # if both line finders are not detecting, the rover is straddling the line and should continue straight
         if grovepi.digitalRead(R_LF_PORT) == 0 and grovepi.digitalRead(L_LF_PORT) == 0:
             print("no lines detected")
             mv.turn(0)
             mv.forward(speed)
+        # if both lines are detected, then we are at a junction (or something bad)
         if grovepi.digitalRead(R_LF_PORT) == 1 and grovepi.digitalRead(L_LF_PORT) == 1:\
             print("both lines detected, something BAD has happened")
+        # turn in the direction that the line finder detects a line
         elif grovepi.digitalRead(R_LF_PORT) == 1:
             print("line detected by right sensor")
             mv.turn(-1)
--- script/movement.py
+++ script/movement.py
@@ -1,8 +1,16 @@
+# HEADER
+# Desc: This file serves as a way for logic to control the rover. It provides 
+# simple functions like forward and turn that are highly customizable (speed, 
+# power, angles, etc.). It also has a function allStop() that immediatly stops
+# all motors. This is most useful in the case of motors running erradically
+
+# ---------- IMPORTS ---------- #
 import time
 import brickpi3
 import grovepi
 from script.calibrationVariables import LargeLegoMotor, SmallLegoMotor
 
+# ---------- INITIALIZATION ---------- #
 BP = brickpi3.BrickPi3()
 
 L_MOTOR_PORT = BP.PORT_B
@@ -12,19 +20,24 @@ T_MOTOR_PORT = BP.PORT_A
 
 all_motors = [L_MOTOR_PORT, R_MOTOR_PORT, T_MOTOR_PORT]
 
+# ---------- GLOBAL VARIABLES ---------- #
 BASE_SPEED = 10
 
 turn_state = 0
 
 
+# ---------- CLASSES ---------- #
 class MotorStallException(Exception):
     def __init__(self, *args: object) -> None:
         print("Motor stalled -- erroring")
 
+
+# ---------- FUNCTIONS ---------- #
 # This function can either take a position (cm) for the robot to travel and/or
 # a speed (cm/s) for it to travel at. Default speed is 15 cm/s
-def forward(speed = BASE_SPEED, position = False, dir_change = -1):
-    speed = (-1) * speed * dir_change
+def forward(speed = BASE_SPEED, position = False, dir_change = 1):
+    speed = speed * dir_change # be able to switch directions
+    
     # Error checking for inputted values for position
     if position:
         if type(position) is not int or type(position) is not float:
@@ -47,7 +60,8 @@ def forward(speed = BASE_SPEED, position = False, dir_change = -1):
             # TODO figure out how to set the speed for this operation
             BP.set_motor_position(L_MOTOR_PORT, L_current + position * LargeLegoMotor.delta_encoder_to_position)
             BP.set_motor_position(R_MOTOR_PORT, R_current + position * LargeLegoMotor.delta_encoder_to_position)
-            
+
+    # exceptions call allStop so motors stop
     except KeyboardInterrupt:
         allStop()
         BP.reset_all()
@@ -57,15 +71,25 @@ def forward(speed = BASE_SPEED, position = False, dir_change = -1):
   
 def turn(dir, speed=BASE_SPEED, precise=False): # defining dir=direction 0 to be straight, -1 as right and +1 as left
     # BP.set_motor_limits(T_MOTOR_PORT, 0, 60)
+    
+    # keeps a state for current turn direction so we can work relativly across multiple function calls
     global turn_state
+    
+    # change dir to be relative and update turn state, in place
     dir, turn_state = dir - turn_state, dir
     
+    # set the motor position to desired position
     T_current = BP.get_motor_encoder(T_MOTOR_PORT)
     T_target = T_current + dir * SmallLegoMotor.full_turn_from_zero
     BP.set_motor_position(T_MOTOR_PORT, T_target)
     
+    # runs if we want to WAIT until the turn is complete until we procede
     if precise:
+        # error tells us how far from the desired position we can be
         error = 5
+        
+        # these functions are for turning left or right
+        # they will also slowly increase the rate of error bounds, so at SOME point it will end
         if dir > 0:
             while BP.get_motor_encoder(T_MOTOR_PORT) < T_target - error: # wait until the turn is complete to within 5 degrees
                 print(f"Turn motor is at {BP.get_motor_encoder(T_MOTOR_PORT)}, target is {T_target}")
@@ -81,7 +105,6 @@ def turn(dir, speed=BASE_SPEED, precise=False): # defining dir=direction 0 to be
                 error += 0.2    
                 time.sleep(0.1)
         
-    
 
 def allStop():
     print("Stopping")    
